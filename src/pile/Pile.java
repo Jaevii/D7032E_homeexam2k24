f274cd131e90efbd98f11d323cacfd4009bdf4aa
Ludvig Järvi <98786177+Jaevii@users.noreply.github.com>
2024-10-16 21:45:14 +0200

    pile implementation

--- src/pile/Pile.java
+++ src/pile/Pile.java
@@ -1,5 +1,61 @@
 package src.pile;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
+import src.card.CardInterface;
+
 public class Pile implements PileInterface {
     
+    private ArrayList<CardInterface> cards;
+
+    public Pile(ArrayList<CardInterface> cards) {
+        this.cards = cards;
+    }
+
+    @Override
+    public void shuffle() {
+        Collections.shuffle(this.cards);
+    }
+
+    @Override
+    public CardInterface drawTopCard() {
+        if (cards.isEmpty()) {
+            return null;
+        }
+        return cards.remove(0);
+    }
+
+    @Override
+    public CardInterface drawBottomCard() {
+        if (cards.isEmpty()) {
+            return null;
+        }
+        return cards.remove(cards.size() - 1);
+    }
+
+    @Override
+    public int getSize() {
+        return cards.size();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return cards.isEmpty();
+    }
+
+    @Override
+    public ArrayList<CardInterface> getCards() {
+        return cards;
+    }
+
+    @Override
+    public void addCard(CardInterface card) {
+        this.cards.add(card);
+    }
+
+    @Override
+    public CardInterface getCard(int index) {
+        return this.cards.get(index);
+    }
 }
--- src/pile/PileInterface.java
+++ src/pile/PileInterface.java
@@ -1,5 +1,19 @@
 package src.pile;
 
+import java.util.ArrayList;
+import src.card.CardInterface;
+
 public interface PileInterface {
-    
+
+    public void shuffle();
+
+    public CardInterface drawTopCard();
+    public CardInterface drawBottomCard();
+
+    public int getSize();
+    public boolean isEmpty();
+
+    public ArrayList<CardInterface> getCards();
+    public CardInterface getCard(int index);
+    public void addCard(CardInterface card);
 }
